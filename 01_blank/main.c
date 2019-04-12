5bf6bd141527a04dde6d0ed939c96f85dcfaae29
lisovrv <33493692+lisovrv@users.noreply.github.com>
2019-04-12 11:31:49 +0300

    Add files via upload

---
+++ 01_blank/main.c
@@ -0,0 +1,14 @@
+/*
+ * It is just simple and pure template project
+ * It does absolutely nothing and indicates that toolchain
+ * is installed correctly.
+ */
+
+int main(void)
+{
+    do
+    {
+	; // nothing
+    }while(0);
+    return 0;
+}
